52b72d231130a374569e49892846247126f4e50a
Cody Shepherd <cody.shepherd@canonical.com>
2018-12-17 19:52:34 -0800

    WIP: working out side and textbar views for inputting bugs

--- src/main/main.go
+++ src/main/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/jroimartin/gocui"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -18,9 +19,80 @@ func main() {
 	log.SetLevel(log.DebugLevel)
 	log.Info("LP Bug Notifier started")
 
-	// var term *Term = NewTerm()
-	// term.Run()
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer g.Close()
 
-	var ui *Ui = NewUi()
-	ui.Run()
+	g.Cursor = true
+
+	u := NewUi()
+	tb := NewTextBar()
+
+	g.SetManager(tb, u)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlA, gocui.ModNone, tb.Edit); err != nil {
+		log.Panic(err)
+	}
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlQ, gocui.ModNone, fgUi); err != nil {
+		log.Panic(err)
+	}
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panic(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panic(err)
+	}
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
+
+func fgUi(g *gocui.Gui, v *gocui.View) error {
+	g.SetCurrentView("side")
+	return nil
+}
+
+func fgTextBar(g *gocui.Gui, v *gocui.View) error {
+	g.SetCurrentView("TextBar")
+	return nil
+}
+
+func cursor_keybindings(g *gocui.Gui, v *gocui.View) {
+	if err := g.SetKeybinding("TextBar", gocui.KeyArrowLeft, gocui.ModNone,
+		cursorLeft); err != nil {
+		log.Panic(err)
+	}
+
+	if err := g.SetKeybinding("TextBar", gocui.KeyArrowRight, gocui.ModNone,
+		cursorRight); err != nil {
+		log.Panic(err)
+	}
+}
+
+func cursorRight(g *gocui.Gui, v *gocui.View) error {
+	if v != nil {
+		ox, _ := v.Origin()
+		cx, _ := v.Cursor()
+		if cx+1 < ox+10 {
+			v.MoveCursor(1, 0, false)
+		}
+	}
+	return nil
+}
+
+func cursorLeft(g *gocui.Gui, v *gocui.View) error {
+	if v != nil {
+		ox, _ := v.Origin()
+		cx, _ := v.Cursor()
+		if cx-1 >= ox {
+			v.MoveCursor(-1, 0, false)
+		}
+	}
+	return nil
 }
--- src/main/ui.go
+++ src/main/ui.go
@@ -2,52 +2,144 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"time"
 
 	"github.com/jroimartin/gocui"
-	log "github.com/sirupsen/logrus"
+	//log "github.com/sirupsen/logrus"
 )
 
 type Ui struct {
-	gui  *gocui.Gui
-	term *Term
+	// gui     *gocui.Gui
+	tracker *Tracker
 }
 
 func NewUi() *Ui {
 	u := new(Ui)
-	var err error
-	u.gui, err = gocui.NewGui(gocui.OutputNormal)
-	if err != nil {
-		log.Panic(err)
-	}
+	u.tracker = NewTracker()
+	u.tracker.Read()
+	/*
+		var err error
+		u.gui, err = gocui.NewGui(gocui.OutputNormal)
+		if err != nil {
+			log.Panic(err)
+		}
 
-	u.gui.SetManagerFunc(layout)
+		u.gui.SetManagerFunc(u.MkLayout())
 
-	if err := u.gui.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panic(err)
-	}
-
+		if err := u.gui.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+			log.Panic(err)
+		}
+	*/
 	return u
 }
 
-func (u *Ui) Run() {
-	defer u.gui.Close()
+/*
+// "curried" function for passing term contents into layout
+func (u *Ui) MkLayout() func(*gocui.Gui) error {
+	return func(g *gocui.Gui) error {
+		_, maxY := g.Size()
+		if v, err := g.SetView("side", -1, -1, 30, maxY); err != nil {
+			if err != gocui.ErrUnknownView {
+				return err
+			}
+			v.Highlight = true
+			v.SelBgColor = gocui.ColorGreen
+			v.SelFgColor = gocui.ColorBlack
+			var noargs []string
+			bugs := list_bugs(u.tracker, noargs)
 
-	if err := u.gui.MainLoop(); err != nil && err != gocui.ErrQuit {
-		log.Panic(err)
+			for _, line := range bugs {
+				fmt.Fprintln(v, line)
+			}
+		}
+		return nil
 	}
 }
+*/
 
-func layout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-	if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+func (u *Ui) Layout(g *gocui.Gui) error {
+	_, maxY := g.Size()
+	if v, err := g.SetView("side", -1, -1, 30, maxY); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		fmt.Fprintln(v, "Hello world!")
+		v.Highlight = true
+		v.SelBgColor = gocui.ColorGreen
+		v.SelFgColor = gocui.ColorBlack
+		var noargs []string
+		bugs := list_bugs(u.tracker, noargs)
+
+		for _, line := range bugs {
+			fmt.Fprintln(v, line)
+		}
+	}
+	return nil
+
+}
+
+type TextBar struct {
+	buffer []string
+}
+
+func NewTextBar() *TextBar {
+	tb := new(TextBar)
+	return tb
+}
+
+func (tb *TextBar) Layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	v, err := g.SetView("TextBar", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2)
+	if err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, "Input Bug")
+		cursor_keybindings(g, v)
 	}
 	return nil
 }
 
-func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
+func (tb *TextBar) Edit(g *gocui.Gui, v *gocui.View) error {
+	_, err := g.SetCurrentView("TextBar")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func add_bug(t *Tracker, args []string) {
+
+}
+
+func list_bugs(t *Tracker, args []string) []string {
+	buffer := []string{}
+	t.lock.Lock()
+	for k, v := range t.list {
+		tm, err := time.Parse(time.RFC3339, v.BugStruct.Date_last_message)
+
+		// check for updated-ness and prepend string if appropriate
+		// note that once "Updated!" is displayed, the Changed flag on the
+		// bug will be turned off
+		updated := ""
+		if v.Changed {
+			updated = "**Updated!** "
+			v.Changed = false
+		}
+
+		// check for time conversion failure
+		if err != nil {
+			buffer = append(buffer, fmt.Sprintf("%s%s: %s", updated, k,
+				v.BugStruct.Title))
+		} else {
+			buffer = append(buffer, fmt.Sprintf("%s%s [%s]: %s", updated,
+				k, time.Since(tm).Truncate(time.Minute).String(), v.BugStruct.Title))
+		}
+	}
+	t.lock.Unlock()
+
+	// sort ascending by time since last update
+	sort.Sort(byTime(buffer))
+	return buffer
 }
